676d20fdc92c02374582f67ccd751aa783538a95
Louis Hollingworth <louis@aplicared.co>
2019-05-28 21:47:18 +0100

    App now adds to the db

--- lib/ui/listis_screen.dart
+++ lib/ui/listis_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:listis/model/listis_item.dart';
+import 'package:listis/util/db_client.dart';
+import 'package:sqflite/sqlite_api.dart';
 
 class ListisScreen extends StatefulWidget {
   @override
@@ -6,12 +9,25 @@ class ListisScreen extends StatefulWidget {
 }
 
 class _ListisScreenState extends State<ListisScreen> {
+  final _editTextController = TextEditingController();
+
+  var db = new DatabaseHelper();
+
+  void _handleSubmit(String text) async {
+    _editTextController.clear();
+
+    ListisItem listisItem =
+        new ListisItem(text, DateTime.now().toIso8601String());
+    int savedItemID = await db.saveItem(listisItem);
+
+    print(savedItemID);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
       body: Column(),
-
       floatingActionButton: new FloatingActionButton(
         tooltip: "Add item",
         backgroundColor: Colors.blueAccent,
@@ -24,5 +40,39 @@ class _ListisScreenState extends State<ListisScreen> {
   }
 
   void _showFormDialog() {
+    var alert = new AlertDialog(
+      content: new Row(
+        children: <Widget>[
+          new Expanded(
+            child: new TextField(
+              controller: _editTextController,
+              autofocus: true,
+              decoration: new InputDecoration(
+                  labelText: "Item",
+                  hintText: "e.g. Don't buy stuff",
+                  icon: new Icon(Icons.note_add)),
+            ),
+          )
+        ],
+      ),
+      actions: <Widget>[
+        new FlatButton(
+            onPressed: () {
+              _handleSubmit(_editTextController.text);
+              _editTextController.clear();
+            },
+            child: Text("Save")),
+        new FlatButton(
+          onPressed: () => Navigator.pop(context),
+          child: Text("Cancel"),
+        )
+      ],
+    );
+
+    showDialog(
+        context: context,
+        builder: (_) {
+          return alert;
+        });
   }
 }
